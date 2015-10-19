532fd9231dd3ad308730aa47c46198cfe2006482
darrylnu <darrylnu@gmail.com>
2015-10-19 12:34:48 -0700

    First commit, passed 2 tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,45 @@
 // YOUR CODE HERE:
 
+
+
+var message = {
+  username: 'Darryl',
+  text: 'tga is awesome',
+  roomname: '4chan'
+};
+
+
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/chatterbox',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message');
+//   }
+// });
+
+var app = {
+  init: function(){},
+  send: $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+})
+
+};
